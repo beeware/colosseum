e05b90545179bb63dfb136c66e4de4d374e59054
Russell Keith-Magee <russell@keith-magee.com>
2017-09-28 08:28:19 +0800

    Clear the deck of old tests and references.

--- tests/w3c/css-flexbox-1/reference/test_w3c.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-from tests.utils import LayoutEngineTestCase, build_w3c_test
-
-
-class ReferenceTestCase(LayoutEngineTestCase):
-    test_flex_vertical_align_effect = build_w3c_test('css-flexbox-1/reference/flex-vertical-align-effect.json')
