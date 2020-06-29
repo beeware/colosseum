3f1443dcc7f993612bd6f56e4a0b8f070b930f3f
fanninpm <fanninpm@miamioh.edu>
2020-06-28 20:17:00 -0400

    Specify package name and version in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+import io
+import re
 from setuptools import setup
 
-setup()
+# Setuptools currently balks at reading the name
+# and version from setup.cfg.
+
+with io.open('./src/colosseum/__init__.py', encoding='utf8') as version_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(name='colosseum', version=version)
