5ca192588d157e3d706406f6ac6f3512d19827c0
Padraic Fanning <fanninpm@miamioh.edu>
2020-07-05 21:49:59 -0400

    Reduce setup.py to use latest setuptools
    Signed-off-by: Padraic Fanning <fanninpm@miamioh.edu>

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python
-import io
-import re
 from setuptools import setup
 
-# Setuptools currently balks at reading the name
-# and version from setup.cfg.
-
-with io.open('./src/colosseum/__init__.py', encoding='utf8') as version_file:
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
-    if version_match:
-        version = version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-setup(name='colosseum', version=version)
+setup()
